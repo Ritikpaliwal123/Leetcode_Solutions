fe6a256d6dec2a0502d93a11773c8255c8d91b73
Ritik Paliwal <rpaliwal0101@gmail.com>
2026-09-16 23:59:39 +0530

    Time: 0 ms (100%), Space: 44.7 MB (48.47%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -1,10 +1,22 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-  for(int i=0;i<nums.length;i++){
-    if(nums[i]>=target){
-        return i;
-    }
-  }
-  return nums.length;
+//   for(int i=0;i<nums.length;i++){
+//     if(nums[i]>=target){
+//         return i;
+//     }
+//   }
+//   return nums.length;
+//     }
+// }
+        int left = 0,right = nums.length;
+        while(left<right){
+            int mid = left+(right-left)/2;
+            if(nums[mid]>=target){
+                right = mid;
+            }else {
+                left = mid + 1;
+            }
+        }
+        return left;
     }
 }
